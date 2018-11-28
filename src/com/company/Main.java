0f0970e5ca8228a7ea93f47302e91969036dc6db
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2018-11-27 20:50:41 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        MonteCarlo mcObj = new MonteCarlo(5, 3, 2);
+
+        int cirCount = 0;
+        int sqrCount = 0;
+
+        for (int i = 0; i < 100000; i++) {
+            if (mcObj.insideCircle(mcObj.nextRainDrop_x(), mcObj.nextRainDrop_y()))
+                cirCount++;
+            sqrCount++;
+        }
+
+        double calc = cirCount * (Math.pow(mcObj.h * 2, 2)) / (sqrCount * Math.pow(mcObj.r, 2));
+        System.out.println(calc);
     }
 }
